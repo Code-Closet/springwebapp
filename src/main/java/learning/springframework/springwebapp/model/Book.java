a3656e31434f9d87f3aa83785259140c50940871
sreekanth207 <sreekanth.pullat@visualiq.com>
2017-12-17 11:24:58 +0530

    Added default constructor to the model. This is very important to add the default constructor in the model, else the spring will throw error when we are trying to access the data base.

--- src/main/java/learning/springframework/springwebapp/model/Book.java
+++ src/main/java/learning/springframework/springwebapp/model/Book.java
@@ -21,6 +21,8 @@ public class Book {
     inverseJoinColumns = @JoinColumn(name = "author_id"))
     Set<Author> authors = new HashSet<>();
 
+    public Book(){}
+
     public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
